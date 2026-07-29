de14db065cc30cee5d4f4e2c6a5115fa6cc1f812
Demetrius Prophette <demetrius.prophette01@student.csulb.edu>
2026-07-28 22:45:54 -0700

    Complete Version 1 rocket flight simulator with visualization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rocket import Rocket
+from simulation import run_simulation
 
 test_rocket = Rocket(
     name="Test Rocket" ,
@@ -11,30 +12,34 @@ test_rocket = Rocket(
     propellant_type="solid"
 )
 
-print(test_rocket.name)
-print(test_rocket.mass)
-print(test_rocket.frontal_area())
-
-from forces import weight_force
-weight = weight_force(test_rocket, 9.81)
-print(weight)
-
-from forces import drag_force
-drag =drag_force(test_rocket, -10, 10)
-print(drag)
-
-from forces import thrust_force
-thrust=thrust_force(test_rocket, 3.2)
-print(thrust)
-
-from forces import net_force
-net=net_force(thrust, weight, drag)
-print(net)
-
-from forces import acceleration
-xcel=acceleration(test_rocket, net)
-print(xcel)
-
-from simulation import run_simulation
 time_history,altitude_history, velocity_history, acceleration_history, impact_velocity = run_simulation(test_rocket)
-print(time_history,altitude_history, velocity_history, acceleration_history, impact_velocity,impact_velocity)
+print("Rocket Flight Summary")
+print("----------------------")
+print(f"Rocket Name:", test_rocket.name)
+print(f"Flight Time: {time_history[-1]:.2f} s")
+print(f"Maximum Altitude: {max(altitude_history):.2f} m")
+print(f"Maximum Velocity: {max(velocity_history):.2f} m/s")
+print(f"Maximum Acceleration: {max(acceleration_history):.2f} m/s^2")
+print(f"Impact Velocity: {impact_velocity:.2f} m/s")
+
+import matplotlib.pyplot as plt
+plt.plot(time_history, altitude_history)
+plt.title("Altitude vs Time")
+plt.xlabel("Time(s)")
+plt.ylabel("Altitude (m)")
+plt.grid(True)
+plt.show()
+
+plt.plot(time_history, velocity_history)
+plt.title("Velocity vs Time")
+plt.xlabel("Time(s)")
+plt.ylabel("Velocity (m/s)")
+plt.grid(True)
+plt.show()
+
+plt.plot(time_history, acceleration_history)
+plt.title("Acceleration vs Time")
+plt.xlabel("Time(s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.grid(True)
+plt.show()
--- simulation.py
+++ simulation.py
@@ -26,24 +26,33 @@ def run_simulation(rocket):
         #Calculate Acceleration
         xcel = acceleration(rocket, net)
         #Update velocity,altitude, and time
-        velocity+= xcel * TIME_STEP
-        altitude+= velocity * TIME_STEP
-        time+=TIME_STEP
-        if launched and altitude <=0:
+        velocity += xcel * TIME_STEP
+        altitude += velocity * TIME_STEP
+        time += TIME_STEP
+        if launched and altitude <= 0:
             impact_velocity = velocity
             altitude = 0
-            velocity= 0
-        #Rocket Flight Data
+
+            time_history.append(time)
+            altitude_history.append(altitude)
+            velocity_history.append(impact_velocity)
+            acceleration_history.append(xcel)
+
+            break
+
         time_history.append(time)
         altitude_history.append(altitude)
         velocity_history.append(velocity)
         acceleration_history.append(xcel)
+
         if altitude > 0:
             launched = True
 
-    return (time_history, altitude_history, velocity_history, acceleration_history,impact_velocity)
-
-        
-
-
-
+    return (
+        time_history,
+        altitude_history,
+        velocity_history,
+        acceleration_history,
+        impact_velocity,
+    )
+       
